cdad37c3ff398b54ef7a91ec2f4e255d0f8f83a9
Bart Trojanowski <bart@jukie.net>
2012-04-07 11:48:04 -0400

    move parsing helpers out of the parser

--- include/0x10c_parser.h
+++ include/0x10c_parser.h
@@ -7,4 +7,98 @@
 
 extern int x10c_parse_line(x10c_op_t *op, const char *buf, size_t buf_len);
 
+// ---- parsing helpers ----
+
+// find a non space
+static inline char * find_non_space(char *p)
+{
+	while (*p && isspace(*p))
+		p++;
+
+	return p;
+}
+
+// find some space
+static inline char * find_space(char *p)
+{
+	while (*p && !isspace(*p))
+		p++;
+
+	return p;
+}
+
+static inline char * find_match(char *p, const char *match)
+{
+	while (*p && !strchr(match, *p))
+		p++;
+
+	return p;
+}
+
+static inline char * find_no_match(char *p, const char *match)
+{
+	while (*p && strchr(match, *p))
+		p++;
+
+	return p;
+}
+
+// return word start, advance pp to end of word */
+static inline char * find_word(char **pp)
+{
+	char *w, *e;
+
+	w = find_non_space(*pp);
+	if (!*w)
+		return NULL;
+
+	e = find_space(w);
+	if (w == e)
+		return NULL;
+
+	*(e++) = 0;
+
+	*pp = find_non_space(e);
+
+	return w;
+}
+
+/* like find_word, but uses a special separator */
+static inline char * find_word_sep(char **pp, const char *sep)
+{
+	char *w, *e;
+
+	w = find_non_space(*pp);
+	if (!*w)
+		return NULL;
+
+	e = find_match(w, sep);
+	if (w == e)
+		return NULL;
+
+	*(e++) = 0;
+
+	*pp = find_no_match(e, sep);
+
+	return w;
+}
+
+// trim leading and trailing space
+static inline char * trim(char *p)
+{
+	char *e;
+	e = p = find_non_space(p);
+
+	while (*e) e++;
+	if (e == p)
+		return p;
+
+	e--;
+	while(e>p && isspace(*e)) e--;
+	e[1] = 0;
+
+	return p;
+}
+
+
 #endif // __included_0x10c_parser_h__
--- lib/0x10c_parser.c
+++ lib/0x10c_parser.c
@@ -17,99 +17,6 @@
 
 // ----
 
-// find a non space
-static char * find_non_space(char *p)
-{
-	while (*p && isspace(*p))
-		p++;
-
-	return p;
-}
-
-// find some space
-static char * find_space(char *p)
-{
-	while (*p && !isspace(*p))
-		p++;
-
-	return p;
-}
-
-static char * find_match(char *p, const char *match)
-{
-	while (*p && !strchr(match, *p))
-		p++;
-
-	return p;
-}
-
-static char * find_no_match(char *p, const char *match)
-{
-	while (*p && strchr(match, *p))
-		p++;
-
-	return p;
-}
-
-// return word start, advance pp to end of word */
-static char * find_word(char **pp)
-{
-	char *w, *e;
-
-	w = find_non_space(*pp);
-	if (!*w)
-		return NULL;
-
-	e = find_space(w);
-	if (w == e)
-		return NULL;
-
-	*(e++) = 0;
-
-	*pp = find_non_space(e);
-
-	return w;
-}
-
-/* like find_word, but uses a special separator */
-static char * find_word_sep(char **pp, const char *sep)
-{
-	char *w, *e;
-
-	w = find_non_space(*pp);
-	if (!*w)
-		return NULL;
-
-	e = find_match(w, sep);
-	if (w == e)
-		return NULL;
-
-	*(e++) = 0;
-
-	*pp = find_no_match(e, sep);
-
-	return w;
-}
-
-// trim leading and trailing space
-static char * trim(char *p)
-{
-	char *e;
-	e = p = find_non_space(p);
-
-	while (*e) e++;
-	if (e == p)
-		return p;
-
-	e--;
-	while(e>p && isspace(*e)) e--;
-	e[1] = 0;
-
-	return p;
-}
-
-// ----
-
 static int x10c_parser_arg(const struct x10c_isn *isn,
 			x10c_op_t *op, int *next_word, char *arg, int arg_num)
 {
@@ -306,6 +213,19 @@ int x10c_parse_line(x10c_op_t *op, const char *buf, size_t buf_len)
 	if (!w) {
 		rc = -EINVAL;
 		goto bail;
+	}
+
+	switch (*w) {
+	case 0:
+	case ';':
+	case '#':
+		return 0;
+	case ':':
+		// later
+		break;
+	default:
+		// needs more parsing
+		break;
 	}
 
 	isn = x10c_lookup_isn_for_name(w);
