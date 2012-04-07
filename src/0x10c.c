451217dfda258c39ef4e6f2ce76fa7db3360ba92
Bart Trojanowski <bart@jukie.net>
2012-04-07 11:38:04 -0400

    make the 0x10c program read from a file

--- src/0x10c.c
+++ src/0x10c.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "0x10c_op.h"
 #include "0x10c_isn.h"
@@ -11,47 +12,36 @@ int main(int argc, char *argv[])
 {
 	int rc;
 	x10c_op_t op;
-	struct {
-		const char buf[128];
-	} tests[] = {
-		{ .buf = "SET A, 0x30" },
-		{ .buf = "SET [0x1000], 0x20" },
-		{ .buf = "SUB A, [0x1000]" },
-		{ .buf = "IFN A, 0x10" },
-		{ .buf = "SET PC, crash" },
-		{ .buf = "SET I, 10" },
-		{ .buf = "SET A, 0x2000" },
-		{ .buf = "SET [0x2000+I], [A]" },
-		{ .buf = "SUB I, 1" },
-		{ .buf = "IFN I, 0" },
-		{ .buf = "SET PC, loop" },
-		{ .buf = "SET X, 0x4" },
-		{ .buf = "JSR testsub" },
-		{ .buf = "SET PC, crash" },
-		{ .buf = "SHL X, 4" },
-		{ .buf = "SET PC, POP" },
-		{ .buf = "SET PC, crash" },
-		{ },
-	}, *t;
+	char buf[1024];
+	FILE *f;
 
-	for (t=tests; *t->buf; t++) {
+	f = fopen(argv[1], "r");
+	if (!f) {
+		fprintf(stderr, "%s: %s\n",
+				argv[1], strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	while ( fgets(buf, sizeof(buf), f) ) {
+
+
 		memset(&op, 0, sizeof(op));
-		rc = x10c_parse_line(&op, t->buf, strlen(t->buf));
+		rc = x10c_parse_line(&op, buf, strlen(buf));
 		switch(rc) {
 		case 1:
-			printf("%-20s ; %04x\n", t->buf,
+			printf("%-20s ; %04x\n", buf,
 					op.word[0]);
 			break;
 		case 2:
-			printf("%-20s ; %04x %04x\n", t->buf,
+			printf("%-20s ; %04x %04x\n", buf,
 					op.word[0], op.word[1]);
 			break;
 		case 3:
-			printf("%-20s ; %04x %04x %04x\n", t->buf,
+			printf("%-20s ; %04x %04x %04x\n", buf,
 					op.word[0], op.word[1], op.word[2]);
 			break;
 		default:
-			printf("%-20s ; rc=%d\n", t->buf, rc);
+			printf("%-20s ; rc=%d\n", buf, rc);
 			return rc;
 		}
 	}
