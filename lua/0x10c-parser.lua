d880d13d4d45782e96374a6b7c373d442a887fbf
Bart Trojanowski <bart@jukie.net>
2012-04-13 17:55:14 -0400

    lua: move grammar into lua/dcpu16.lua

--- lua/0x10c-parser.lua
+++ lua/0x10c-parser.lua
@@ -1,163 +1,32 @@
 #!/usr/bin/lua
 
-local lpeg = require 'lpeg'
-local locale = lpeg.locale();
-local P, R, S, C = lpeg.P, lpeg.R, lpeg.S
-local C, Cc, Ct, Cg = lpeg.C, lpeg.Cc, lpeg.Ct, lpeg.Cg
-
 package.path = './lua/?.lua;' .. package.path
+local lpeg = require 'lpeg'
 local D = require 'dcpu16'
 
+require 'dumper'
+function dump(...)
+        print(DataDumper(...), "\n---")
+end
 
-local debug_level = 0
-local function dbg(l,...)
-        if debug_level >= l then
-                io.stderr:write("# "..table.concat({...},"\t").."\n")
+--
+
+function test()
+        function parse(program)
+                dump(lpeg.match(D.grammar, program), program..'  =>  ')
         end
-end
-local function dbgf(l,fmt,...)
-        if debug_level >= l then
-                io.stderr:write("# "..string.format(fmt,...).."\n")
-        end
-end
-local function die(...)
-        io.stderr:write("ERROR: "..table.concat({...},"\t").."\n")
-        os.exit(1)
-end
-local function lmap(func, array)
-        local new_array = {}
-        for i,v in ipairs(array) do
-                new_array[i] = func(v)
-        end
-        return new_array
-end
-local function tmap(func, array)
-        local new_array = {}
-        for k,v in pairs(array) do
-                new_array[k] = func(v)
-        end
-        return new_array
-end
-local function xx(num)
-        return string.format("%02x", num)
-end
-local function xxxx(num)
-        return string.format("%04x", num)
+        parse ('SET A, B')
+        parse ('SET A, 1')
+        parse ('SET A, [A]')
+        parse ('SET A, [1]')
+        parse ('SET A, [A+1]')
+        parse ('SET A, [1+A]')
+        parse ('JSR A')
+        parse (':x JSR A')
+        parse (':x JSR POP')
+        parse ('; xxx')
+        parse ('SET A, 1; 123')
+        parse ('SET A, 1 ;123')
 end
 
--- parser
-
-local parser = {
-        finalize = function(self)
-                os.exit(0)
-        end,
-        dump = function(self, file)
-                os.exit(0)
-        end,
-        output = function(self, file)
-                os.exit(0)
-        end,
-}
-
-local parser_actions = {
-        comment  = function(...) print('# COMMENT', ...) end,
-        literal  = function(...) print('# LITERAL', ...) end,
-        comma    = function(...) print('# COMMA', ...) end,
-        gop      = function(...) print('# GOP', ...) end,
-        sop      = function(...) print('# SOP', ...) end,
-        greg     = function(...) print('# GREG', ...) end,
-        sreg     = function(...) print('# SREG', ...) end,
-        mref     = function(...) print('# MREF', ...) end,
-        variable = function(...) print('# VAR', ...) end,
-        label    = function(...) print('# LABEL', ...) end
-}
-
-parser.matcher = ( D.comment       / parser_actions.comment
-                + D.literal        / parser_actions.literal
-                + P","           / parser_actions.comma
-                + C( D.gop )       / parser_actions.gop
-                + C( D.sop )       / parser_actions.sop
-                + D.greg_c         / parser_actions.greg
-                + C( D.sreg )      / parser_actions.sreg
-                + D.mref_c         / parser_actions.mref
-                + C( D.variable )  / parser_actions.variable
-                + C( D.label )     / parser_actions.label
-                + C( S'\n' ) / function(...) print('') end
-                + D.whitespace
-                )^0
-
-local opargs = { }
-local parser_oparg = C( D.oparg ) / function(...) 
-        table.insert(opargs, ...)
-end
-local parser_gisn = C(D.gop) * D.w1 * parser_oparg * D.w0 * D.comma * D.w0 * parser_oparg
-
-parser.matcher = ( D.comment       / function(...) print('') end
-                 + parser_gisn   / function(...)
-                         print('gisn '..(...)..' '..table.concat(opargs,' '))
-                         opargs = {}
-                 end
-                 + D.xisn_c        / function(...) print('xisn') end
-                 + D.label_c       / function(...) print('label') end
-                 + C( S'\n' )    / function(...) print('') end
-                 + D.whitespace
-                 )^0
-
-
-
-
-
--- parse command line
-
-local filename
-local handler
-
-local i = 1
-while i<=#arg do
-        local a = arg[i]
-        i = i + 1
-        if a == "-h" or a == "--help" then
-                print '0x10c'
-                print ''
-                print ' -h --help                         - this help'
-                print ' -v --verbose                      - be more verbose'
-                print ' -p --parser <file>                - run parser on a file'
-                print ' -o --output <file>                - write output here'
-                os.exit(0)
-
-        elseif a == "-v" or a == "--verbose" then
-                debug_level = debug_level + 1
-
-        elseif a == "-p" or a == "--parse" then
-                handler = parser
-                filename = arg[i]
-                i = i + 1
-
-        elseif a == "-o" or a == "--output" then
-                output = arg[i]
-                i = i + 1
-
-        else
-                die("unknown option " .. a)
-        end
-end
-
-if not handler then die("need to specify an action; see --help") end
-if not filename then die("need to specify a file") end
-
-
--- frontend
-
-local fh = assert(io.open(filename))
-local input = fh:read'*a'
-fh:close()
-local rc = lpeg.match(handler.matcher, input)
-if (rc < input:len()) then
-        print("ERROR: parser filed here...")
-        print(input:sub(rc))
-end
-
-handler:dump()
-handler:finalize()
-handler:output(output)
-
+test()
--- lua/dcpu16.lua
+++ lua/dcpu16.lua
@@ -2,36 +2,39 @@
 
 local lpeg = require 'lpeg'
 local locale = lpeg.locale();
-local P, R, S, C = lpeg.P, lpeg.R, lpeg.S
-local C, Cc, Ct, Cg = lpeg.C, lpeg.Cc, lpeg.Ct, lpeg.Cg
+local P, R, S, V = lpeg.P, lpeg.R, lpeg.S, lpeg.V
+local C, Cc, Ct, Cg, Cf = lpeg.C, lpeg.Cc, lpeg.Ct, lpeg.Cg, lpeg.Cf
 
 module(..., package.seeall)
 
-whitespace = S' \t\v\n\f'
-w0 = whitespace^0
-w1 = whitespace^1
+local whitespace = S' \t\v\n\f'
+local w0 = whitespace^0
+local w1 = whitespace^1
+local comma = w0 * P"," * w0
+local plus = w0 * P"+" * w0
+local colon = w0 * P":" * w0
+local semi = w0 * P";" * w0
+local eol = S '\r\n\f'
+local digit = R'09'
+local letter = R('az', 'AZ') + P'_'
+local alphanum = letter + digit
+local hex = R('af', 'AF', '09')
+local exp = S'eE' * S'+-'^-1 * digit^1
 
-comma = P","
+--local is = S'uUlL'^0
+hexnum = P'0' * S'xX' * hex^1 -- * is^-1
+octnum = P'0' * digit^1 -- * is^-1
+decnum = digit^1 -- * is^-1
 
-digit = R('09')
-
-digit = R'09'
-letter = R('az', 'AZ') + P'_'
-alphanum = letter + digit
-hex = R('af', 'AF', '09')
-exp = S'eE' * S'+-'^-1 * digit^1
-fs = S'fFlL'
-is = S'uUlL'^0
-
-hexnum = P'0' * S'xX' * hex^1 * is^-1
-octnum = P'0' * digit^1 * is^-1
-decnum = digit^1 * is^-1
-floatnum = digit^1 * exp * fs^-1 +
+--[[
+local fs = S'fFlL'
+local floatnum = digit^1 * exp * fs^-1 +
                  digit^0 * P'.' * digit^1 * exp^-1 * fs^-1 +
                  digit^1 * P'.' * digit^0 * exp^-1 * fs^-1
-numlit = hexnum + octnum + floatnum + decnum
+]]--
+numlit = hexnum + octnum + decnum -- + floatnum
 
-numlit_cg = Cg(numlit, "numlit")
+--numlit_cg = Cg(numlit, "numlit")
 
 charlit =
   P'L'^-1 * P"'" * (P'\\' * P(1) + (1 - S"\\'"))^1 * P"'"
@@ -81,7 +84,7 @@ greg = (
   P"I" +
   P"J"
 )
-greg_c = C( greg )
+--greg_c = C( greg )
 
 -- "special register"
 sreg = (
@@ -92,10 +95,10 @@ sreg = (
   P"PC" +
   P"O"
 )
-sreg_c = C( sreg )
+--sreg_c = C( sreg )
 
 reg = greg + sreg
-reg_cg = Cg(reg, "reg")
+--reg_cg = Cg(reg, "reg")
 
 -- "memory reference"
 mref = P"[" * w0 * (
@@ -104,14 +107,16 @@ mref = P"[" * w0 * (
                     + ( reg )
                     + ( numlit )
                   ) * w0 * P"]"
-mref_c = C( mref )
+--mref_c = C( mref )
 
+--[[
 mref_ct = Ct( P"[" * w0 * (
                       ( reg_cg * w0 * P"+" * w0 * numlit_cg )
                     + ( numlit_cg * w0 * P"+" * w0 * reg_cg )
                     + ( reg_cg )
                     + ( numlit_cg )
                   ) * w0 * P"]" )
+]]--
 
 -- symbolic stuff: keywords, variables, lables, etc
 keywords = gop + sop + reg
@@ -120,20 +125,60 @@ variable = (locale.alpha + P "_") * (locale.alnum + P "_")^0 - ( keywords )
 
 label = P":" * variable
 
-label_c = P":" * C( variable )
+--label_c = P":" * C( variable )
 
 -- instruction opcodes take arguments
 
 oparg = reg + numlit + variable + mref
-oparg_c = C( oparg )
+--oparg_c = C( oparg )
 
 -- "generic instruction"
-gisn_c = C(gop) * w1 * C(oparg) * w0 * comma * w0 * oparg_c
+--gisn_c = C(gop) * w1 * C(oparg) * w0 * comma * w0 * oparg_c
 
 -- "extended instruction"
 jsr_arg = numlit + variable
 jsr_isn_c = C(jsr_op) * w1 * C(jsr_arg)
 xisn_c = jsr_isn_c
 
-isn_c = gisn_c + xisn_c
+--isn_c = gisn_c + xisn_c
+
+-- the full grammar
+
+local _comment = V'comment'
+local _lisn = V'lisn'
+local _isn = V'isn'
+local _gisn = V'gisn'
+local _sisn = V'sisn'
+local _label = V'label'
+local _var = V'var'
+local _gop = V'gop'
+local _sop = V'sop'
+local _oparg = V'oparg'
+local _reg = V'reg'
+local _greg = V'greg'
+local _sreg = V'sreg'
+local _num = V'num'
+local _mrefp = V'mrefp'
+local _mref = V'mref'
+
+grammar = lpeg.P{ 'line',
+        line    = _lisn^-1 * w0 * _comment^-1;
+        lisn    = Ct(_label * w1 * _isn) + _isn;
+        comment = C(semi * (1 - eol)^0);
+        isn     = _gisn + _sisn;
+        gisn    = Ct(_gop * w1 * _oparg * comma * _oparg);
+        sisn    = Ct(_sop * w1 * _oparg);
+        label   = C(colon * variable);
+        var     = C(variable);
+        gop     = C(gop);
+        sop     = C(sop);
+        oparg   = _reg + _num + _mref;
+        reg     = _greg + _sreg;
+        greg    = C(greg);
+        sreg    = C(sreg);
+        mrefp   = (_greg * plus * _num) + (_num * plus * _greg) + _greg + _num;
+        mref    = Ct(P'['*w0 * _mrefp * w0*P']');
+        num     = numlit / tonumber;
+}
+
 
