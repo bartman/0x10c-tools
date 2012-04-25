60182c7b4a298e77e59271b733a814c0fc687afe
Bart Trojanowski <bart@jukie.net>
2012-04-24 20:21:55 -0400

    capture new registers

--- lua/dcpu16/defs.lua
+++ lua/dcpu16/defs.lua
@@ -16,35 +16,60 @@ DD.generic_registers = {
 }
 
 DD.special_registers = {
-        POP ={ num=0x18 },
-        PEEK={ num=0x19 },
-        PUSH={ num=0x1a },
-        SP  ={ num=0x1b },
-        PC  ={ num=0x1c },
-        O   ={ num=0x1d },
+        POP ={ num=0x18, a=1,             },    -- [SP++]
+        PUSH={ num=0x18,      b=1         },    -- [--SP]
+        PEEK={ num=0x19, a=1, b=1         },    -- [SP]
+        PICK={ num=0x1a, a=1, b=1         },    -- [SP + next word]
+        SP  ={ num=0x1b, a=1, b=1, mref=1 },
+        PC  ={ num=0x1c, a=1, b=1         },
+        EX  ={ num=0x1d, a=1, b=1         },
 }
 
+function DD.special_regs_marked_with(self, tag)
+        return tmap(
+            function(r)
+                    if r[tag] then
+                            return r
+                    end
+            end, self.special_registers)
+end
+
 DD.generic_opcodes = {
-        SET={ num=0x1 },
-        SET={ num=0x1 },
-        ADD={ num=0x2 },
-        SUB={ num=0x3 },
-        MUL={ num=0x4 },
-        DIV={ num=0x5 },
-        MOD={ num=0x6 },
-        SHL={ num=0x7 },
-        SHR={ num=0x8 },
-        AND={ num=0x9 },
-        BOR={ num=0xa },
-        XOR={ num=0xb },
-        IFE={ num=0xc },
-        IFN={ num=0xd },
-        IFG={ num=0xe },
-        IFB={ num=0xf },
+        SET={ num=0x01, cycles=1 },
+        ADD={ num=0x02, cycles=2 },
+        SUB={ num=0x03, cycles=2 },
+        MUL={ num=0x04, cycles=2 },
+        MLI={ num=0x05, cycles=2 },
+        DIV={ num=0x06, cycles=3 },
+        DVI={ num=0x07, cycles=3 },
+        MOD={ num=0x08, cycles=3 },
+        AND={ num=0x09, cycles=1 },
+        BOR={ num=0x0a, cycles=1 },
+        XOR={ num=0x0b, cycles=1 },
+        SHR={ num=0x0c, cycles=2 },
+        ASR={ num=0x0d, cycles=2 },
+        SHL={ num=0x0e, cycles=2 },
+        --        0x0f
+        IFB={ num=0x10, cycles=2 },
+        IFC={ num=0x11, cycles=2 },
+        IFE={ num=0x12, cycles=2 },
+        IFN={ num=0x13, cycles=2 },
+        IFG={ num=0x14, cycles=2 },
+        IFA={ num=0x15, cycles=2 },
+        IFL={ num=0x16, cycles=2 },
+        IFU={ num=0x17, cycles=2 },
 }
 
 DD.special_opcodes = {
-        JSR={ num=0x1 },
+        JSR={ num=0x01, cycles=3 },
+
+        INT={ num=0x08, cycles=4 },
+        INT={ num=0x09, cycles=1 },
+        INT={ num=0x0a, cycles=1 },
+
+        HWN={ num=0x10, cycles=2 },
+        HWQ={ num=0x11, cycles=4 },
+        HWQ={ num=0x12, cycles=4 },
 }
 
 
