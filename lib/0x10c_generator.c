ee7c1e98345e6500c80c25d0bffba6fe0b863f37
Bart Trojanowski <bart@jukie.net>
2012-04-27 17:59:42 -0400

    fix command disassembler/generator argument order

--- lib/0x10c_generator.c
+++ lib/0x10c_generator.c
@@ -13,8 +13,8 @@
 		p += rc; \
 	})
 
-char * x10c_append_arg(char *p, char *e, const char *prefix, x10c_word arg_desc,
-		const x10c_word **words)
+static char * x10c_append_arg(char *p, char *e, const char *prefix,
+		x10c_word arg_desc, const x10c_word *word)
 {
 	x10c_word ofs;
 	struct x10c_reg *reg;
@@ -41,7 +41,7 @@ char * x10c_append_arg(char *p, char *e, const char *prefix, x10c_word arg_desc,
 
 	case X10C_ARG_MREF_OFS_REG(A) ... X10C_ARG_MREF_OFS_REG(J):
 		/* 0x10-0x17: [next word + register] */
-		ofs = *((*words)++);
+		ofs = *word;
 		reg = x10c_lookup_reg_for_num(arg_desc
 				- X10C_ARG_MREF_OFS_REG_BASE);
 		append("%s[0x%04x+%s]", prefix, ofs, reg->reg_name);
@@ -50,13 +50,13 @@ char * x10c_append_arg(char *p, char *e, const char *prefix, x10c_word arg_desc,
 
 	case X10C_MREF_NEXT_WORD:
 		/* 0x1e: [next word] */
-		ofs = *((*words)++);
+		ofs = *word;
 		append("%s[0x%04x]", prefix, ofs);
 		return p;
 
 	case X10C_NEXT_WORD:
 		/* 0x1f: next word (literal) */
-		ofs = *((*words)++);
+		ofs = *word;
 		append("%s0x%04x", prefix, ofs);
 		return p;
 
@@ -77,11 +77,14 @@ char * x10c_basic_generator(const struct x10c_isn *isn,
 	char *p = buf;
 	char *e = buf + buf_len;
 	const x10c_word *words = op->word + 1;
+	int a_len;
 
 	append("%s", isn->op_name);
 
-	p = x10c_append_arg(p, e, " ",  op->b.b, &words);
-	p = x10c_append_arg(p, e, ", ", op->b.a, &words);
+	a_len = x10c_op_arg_len(op->b.a);
+
+	p = x10c_append_arg(p, e, " ",  op->b.b, words + a_len);
+	p = x10c_append_arg(p, e, ", ", op->b.a, words);
 
 bail:
 	return buf;
@@ -96,7 +99,7 @@ char * x10c_non_basic_generator(const struct x10c_isn *isn,
 
 	append("%s", isn->op_name);
 
-	p = x10c_append_arg(p, e, " ", op->x.a, &words);
+	p = x10c_append_arg(p, e, " ", op->x.a, words);
 
 bail:
 	return buf;
